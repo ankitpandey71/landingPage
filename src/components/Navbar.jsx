2e01a159afb35c1eace81639fc69ec0ec33de6d6
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2024-08-01 22:17:43 +0530

    [FIX] navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,31 +2,39 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center h-20 bg-slate-500">
-      <div className="ml-4">
-        <Link to="/" className="font-sans font-semibold text-2xl size-5">
+    <div className="flex justify-between items-center h-20 bg-slate-500 px-6">
+      <div>
+        <Link to="/" className="font-sans font-semibold text-2xl text-white">
           Shopify
         </Link>
       </div>
-      <div>
-        <nav className="flex list-none space-x-2 mr-4">
-          <li>
-            <Link to="/products">Products</Link>
-          </li>
-          <li>
-            <Link to="/categories">Categories</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/cart">cart</Link>
-          </li>
-          <li>
-            <Link to="/mode">DarkMOde/lightMode</Link>
-          </li>
-        </nav>
-      </div>
+      <nav className="flex list-none space-x-4">
+        <li>
+          <Link to="/products" className="text-white hover:text-gray-300">
+            Products
+          </Link>
+        </li>
+        <li>
+          <Link to="/categories" className="text-white hover:text-gray-300">
+            Categories
+          </Link>
+        </li>
+        <li>
+          <Link to="/login" className="text-white hover:text-gray-300">
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link to="/cart" className="text-white hover:text-gray-300">
+            Cart
+          </Link>
+        </li>
+        <li>
+          <Link to="/mode" className="text-white hover:text-gray-300">
+            DarkMode/LightMode
+          </Link>
+        </li>
+      </nav>
     </div>
   );
 };
