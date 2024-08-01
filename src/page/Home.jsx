f200ef3fd824a3f2e36cc5381f85a9f12bb7855d
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2024-08-01 23:35:40 +0530

    [FIX] home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,4 @@
 import hero from "../assets/hero.png";
-import Footer from "./Footer";
 import OfferSection from "./OfferSection";
 import SlideBox from "./SlideBox";
 import TrendingProducts from "./TrendingProducts";
@@ -12,10 +11,10 @@ const Home = () => {
         {/* Text Section */}
         <div className="w-full md:w-1/2 flex flex-col items-center md:items-start text-center md:text-left p-4 md:p-8">
           <h6 className="text-gray-600 text-sm mb-2">Starting At $999</h6>
-          <h1 className="text-4xl font-bold text-gray-900 mb-2">
+          <h1 className="text-3xl md:text-4xl font-bold text-gray-900 mb-2">
             The Best Notebook Collection 2024
           </h1>
-          <h4 className="text-lg text-gray-700 mb-4">
+          <h4 className="text-base md:text-lg text-gray-700 mb-4">
             Exclusive offer <span className="text-red-500">-10%</span> off this
             week
           </h4>
@@ -24,8 +23,12 @@ const Home = () => {
           </button>
         </div>
         {/* Image Section */}
-        <div className="w-full md:w-1/2 flex justify-center items-center ">
-          <img src={hero} alt="Hero" className="w-full max-w-md h-auto" />
+        <div className="w-full md:w-1/2 flex justify-center items-center mt-6 md:mt-0">
+          <img
+            src={hero}
+            alt="Hero"
+            className="w-full max-w-xs md:max-w-md h-auto"
+          />
         </div>
       </div>
 
